aca3ce19515bdef9e530d55795aa70037e6f5146
Adeir <adeir@uwo.ca>
2023-10-26 17:19:52 -0400

    update member.py, updated main.py and item.py, fixed minor issues and refactored code

--- member.py
+++ member.py
@@ -1,81 +1,96 @@
+# Importing time module for sleep function and custom clear_screen function from shared.py.
 import time
 from shared import clear_screen
 
+# Initializing an empty list to store Member objects.
 members = []
 
-# member class
+# Defining the Member class.
 class Member():
+    # Constructor to initialize a new member object.
     def __init__(self, name, address, phone, transactions=None):
         self.name = name
         self.address = address
         self.phone = phone
-        self.transactions = []
+        self.transactions = []  # Initializing an empty list to store transactions.
 
+    # String representation for printing member details.
     def __str__(self):
         return f'Name: {self.name}\nAddress: {self.address}\nPhone: {self.phone}'
-    
+
+    # Method to update member details.
     def update(self, name, address, phone):
         self.name = name
         self.address = address
         self.phone = phone
 
+# Function to display a message when no members are available.
 def no_members():
     clear_screen()
     print('There are no members yet!')
-    time.sleep(3)
-    member_manage()
+    time.sleep(3)  # Pausing execution for 3 seconds.
+    member_manage()  # Redirecting back to member management menu.
 
+
+# Function to add a new member.
 def add_member():
     clear_screen()
     print('Add Member')
     name = input('Enter name: ')
     address = input('Enter address: ')
     phone = input('Enter phone number: ')
-    members.append(Member(name, address, phone))
-    member_manage()
+    members.append(Member(name, address, phone))  # Creating and appending a new Member object.
+    member_manage()  # Redirecting back to member management menu.
 
+# Function to display and select members.
 def view_members():
     clear_screen()
     print('Members List:')
-    for m in range(0, len(members)):
+    for m in range(0, len(members)):  # Looping through all members.
         print(f'{m + 1}. {members[m].name}')
-    while True:
+    while True:  # Loop until a valid selection is made.
         user_input = int(input('Enter Choice: '))
         if user_input < len(members) + 1 and user_input > 0:
-            return members[user_input - 1]
+            return members[user_input - 1]  # Returning the selected member object.
 
+
+# Function to display a selected member's details.
 def member_details():
     clear_screen()
-    member = view_members()
+    member = view_members()  # Fetching the selected member.
     clear_screen()
     print('Member Details:')
-    print(member)
-    input('Press any key to return...')
-    member_manage()
+    print(member)  # Printing member details.
+    input('Press any key to return...')  # Pause until user proceeds.
+    member_manage()  # Redirecting back to member management menu.
 
+# Function to update a member's details.
 def update_member():
     clear_screen()
-    member = view_members()
+    member = view_members()  # Fetching the selected member.
     clear_screen()
     name = input('Enter name: ')
     address = input('Enter address: ')
     phone = input('Enter phone number: ')
-    member.update(name, address, phone)
-    member_manage()
+    member.update(name, address, phone)  # Updating the member's details.
+    member_manage()  # Redirecting back to member management menu.
 
+# Function to remove a member.
 def remove_member():
     clear_screen()
-    members.remove(view_members())
+    members.remove(view_members())  # Removing the selected member.
     clear_screen()
     print('member has been removed')
-    time.sleep(3)
-    member_manage()
+    time.sleep(3)  # Pausing execution for 3 seconds.
+    member_manage()  # Redirecting back to member management menu.
 
+# Function to view purchase history of a member (commented out parts are placeholders for future code).
 def purchase_history():
-    member = view_members()
+    member = view_members()  # Fetching the selected member.
     # for purchase in member.transactions:
     #     print(purchase)
 
+# Main function for member management options.
 def member_manage():
     clear_screen()
     print('Member Management:')
@@ -85,29 +100,26 @@ def member_manage():
     print('4. Remove Member')
     print('5. View Purchase History')
     print('Any other key to return to the main menu')
-    user_choice = input('Enter Choice: ')
-    if(user_choice == '1'):
+    user_choice = input('Enter Choice: ')  # Getting user's choice.
+    if user_choice == '1':
         add_member()
-    elif(user_choice == '2'):
-        if not members:
-            clear_screen()
-            print('There are no members yet!')
-            time.sleep(3)
-            member_manage()
+    elif user_choice == '2':
+        if not members:  # Check if members list is empty.
+            no_members()
         else:
             update_member()
-    elif(user_choice == '3'):
-        if not members:
+    elif user_choice == '3':
+        if not members:  # Check if members list is empty.
             no_members()
         else:
             member_details()
-    elif(user_choice == '4'):
-        if not members:
+    elif user_choice == '4':
+        if not members:  # Check if members list is empty.
             no_members()
         else:
             remove_member()
-    elif(user_choice == '5'):
-        if not members:
+    elif user_choice == '5':
+        if not members:  # Check if members list is empty.
             no_members()
         else:
             purchase_history()
